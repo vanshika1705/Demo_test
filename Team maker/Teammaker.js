373a0c507ce4919967417c471a37d052c6b5acdc
sonali101 <76722335+sonali101@users.noreply.github.com>
2021-01-12 18:35:27 +0530

    Update Teammaker.js

--- Team maker/Teammaker.js
+++ Team maker/Teammaker.js
@@ -27,6 +27,24 @@ document.getElementsByClassName("add").click= function()
     document.getElementByID(list_item);
     
 }
+//participant list
+var list=[];
+function add(){
+    var name = document.getElementById("text").value ;
+    list.push(name);
+
+    var ul=document.createElement("ul");
+        ul.setAttribute("id","ulvalue");
+            document.getElementById("ulList").appendChild(ul);
+            for(var k=0;k<list.length;k++){
+                var li=document.createElement("li");
+                var y=document.createTextNode(list[k]);
+            }
+            li.appendChild(y);
+            ul.appendChild(li); 
+            
+     document.getElementById("text").value="";
+}
 
 
 /*function reset() {
