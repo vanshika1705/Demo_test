4ad765173584fef5bf4f2483426a530d8e95ebf8
vanshika1705 <76651799+vanshika1705@users.noreply.github.com>
2021-02-05 11:17:04 +0530

    Create script.js

---
+++ Team maker2/script.js
@@ -0,0 +1,27 @@
+const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
+var lbl= document.querySelector(".line");
+
+btnHamburger.addEventListener('click', function(){
+  console.log('click hamburger');
+
+  if(header.classList.contains('open')){ // Close Hamburger Menu
+    lbl.setAttribute("style","display:flex;");
+    header.classList.remove('open');    
+    fadeElems.forEach(function(element){
+      element.classList.remove('fade-in');
+      element.classList.add('fade-out');
+    });
+    
+  }
+  else { // Open Hamburger Menu
+    lbl.setAttribute("style","display:none;");
+    header.classList.add('open');
+    fadeElems.forEach(function(element){
+      element.classList.remove('fade-out');
+      element.classList.add('fade-in');
+    });
+  }  
+});
